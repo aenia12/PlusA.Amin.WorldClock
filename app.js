e5c8d6515c9b8581f7f67f1017a1e1da8fe66a79
aenia12 <aenia12@gmail.com>
2023-11-19 17:33:20 -0600

    Added some JS. Updated index.html. JS not working yet.

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
-function showTime(event) {
+function showTime() {
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  // New York
+  let newYorkElement = document.querySelector("#new-york");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
+
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM	Do YYYY");
+    newYorkTimeElement.innerHTML = newYorkTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+
+
+function showCity(event) {
         if (event.target.value.length > 0) {
           let currentTime = moment().tz(event.target.value).format("dddd, MMMM D, YYYY h:mm A");
 ​
           alert(`It is ${currentTime} in ${event.target.value}`);
       }
     }
-      let citySelect = document.queryselector("#cities");
-      citySelect.addEventListener("change", showTime);
+      
+showTime();
+setInterval (showTime, 1000);
+
+    let citySelect = document.queryselector("#city");
+      citySelect.addEventListener("change", showCity);
